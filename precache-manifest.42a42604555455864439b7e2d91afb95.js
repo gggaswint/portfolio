f5652f99b16d0799e72e321396a05e64c41a8bd6
gggaswint <ggaswint@gmail.com>
2025-02-05 16:14:40 -0800

    copied form old github account

---
+++ precache-manifest.42a42604555455864439b7e2d91afb95.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d35aa8b6405df6eb05af418bf171fa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a9b2fad380d713616288",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "3e20fa0ee2e72a1f8452",
+    "url": "/portfolio/static/js/2.67ef5b22.chunk.js"
+  },
+  {
+    "revision": "69211abf3b49d4ff08de8fc1299580b7",
+    "url": "/portfolio/static/js/2.67ef5b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b2fad380d713616288",
+    "url": "/portfolio/static/js/main.229ee3ac.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "594359967b5bf30af901853a3a6a73ee",
+    "url": "/portfolio/static/media/EWBG.59435996.png"
+  },
+  {
+    "revision": "01094f5ce35dc87ea79a2ba3848e17b0",
+    "url": "/portfolio/static/media/NuReco.01094f5c.pdf"
+  },
+  {
+    "revision": "4314223349d4355b885a90c9899a9eeb",
+    "url": "/portfolio/static/media/NuReco.43142233.png"
+  },
+  {
+    "revision": "74dfeb5b7c9d093d545513aa930b35b2",
+    "url": "/portfolio/static/media/UHEnu.74dfeb5b.png"
+  },
+  {
+    "revision": "2bf673ca32ad6583eec26af9400728ec",
+    "url": "/portfolio/static/media/dodgerMan.2bf673ca.png"
+  },
+  {
+    "revision": "9c767dae7b7e510bf6593e5c5ed53c05",
+    "url": "/portfolio/static/media/horizontalProp.9c767dae.png"
+  },
+  {
+    "revision": "d12bc29431f31a62258700ee2cdc1b32",
+    "url": "/portfolio/static/media/myBestFriend.d12bc294.png"
+  },
+  {
+    "revision": "64781d8c708cb81b8bb3c3099b3d3114",
+    "url": "/portfolio/static/media/nuRadioReco.64781d8c.png"
+  },
+  {
+    "revision": "7536dc713a6fd93e7c2e2b21f705f637",
+    "url": "/portfolio/static/media/nuVertex.7536dc71.png"
+  },
+  {
+    "revision": "88866ad4e79654edbab8f607a9082dee",
+    "url": "/portfolio/static/media/resume.88866ad4.pdf"
+  },
+  {
+    "revision": "71b4763d1ecf20b8594894404e0e1d3d",
+    "url": "/portfolio/static/media/tapThat.71b4763d.png"
+  },
+  {
+    "revision": "251905f71640782575e617640ed4f280",
+    "url": "/portfolio/static/media/whitePaper.251905f7.png"
+  }
+]);
